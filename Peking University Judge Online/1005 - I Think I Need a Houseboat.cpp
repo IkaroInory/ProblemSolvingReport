072df13ff80cd116c6d8326975e0582578ed7518
IkaroInory <ikaroinory233@gmail.com>
2021-08-20 00:32:07 +0800

    2021.8.20

---
+++ Peking University Judge Online/1005 - I Think I Need a Houseboat.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        double x, y;
+        cin >> x >> y;
+        cout << "Property " << i << ": "
+            << "This property will begin eroding in year "
+            << ceil(3.1415926 * (x * x + y * y) / 100) << "." << endl;
+    }
+    cout << "END OF OUTPUT." << endl;
+    return 0;
+}
