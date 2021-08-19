7458468f136e98a1a92c1e5db176be9ba985e434
IkaroInory <ikaroinory233@gmail.com>
2021-08-20 00:26:43 +0800

    2021.8.21

---
+++ LeetCode/12 - Integer to Roman.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+ 
+class Solution {
+public:
+    string intToRoman(int num) {
+        int value[] = { 1000,900,500,400,100,90,50,40,10,9,5,4,1 };
+        string c[] = { "M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I" };
+
+        // Greedy Algorithm
+        string ans = "";
+        for (int i = 0;i < 13;i++) {
+            while (num >= value[i]) {
+                num -= value[i];
+                ans += c[i];
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int> x = {
+        3,
+        4,
+        9,
+        58,
+        1994
+    };
+
+    for (int i = 0; i < x.size(); i++) {
+        cout << "Input: " << x[i] << endl;
+        cout << "Output: " << (new Solution())->intToRoman(x[i]) << endl;
+    }
+    return 0;
+}
---
+++ LeetCode/7 - Reverse Integer.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+ 
+class Solution {
+public:
+    int reverse(int x) {
+        // Exceptional Case.
+        if (x == 0)return 0;
+
+        int abs = 0;
+        while (x) {
+            // Whether abs is in the range of int.
+            if (abs > (pow(2, 31) - 1) / 10 || abs < -pow(2, 31) / 10) return 0;
+            abs *= 10;          // Expand capacity.
+            abs += x % 10;      // Push and Pop.
+            x /= 10;            // Destory.
+        }
+        return abs;
+    }
+};
+
+int main()
+{
+    vector<int> x = {
+        123,
+        -123,
+        120,
+        0,
+        1534236469,
+        -2147483648
+    };
+
+    for (int i = 0; i < x.size(); i++) {
+        cout << "Input: " << x[i] << endl;
+        cout << "Output: " << (new Solution())->reverse(x[i]) << endl;
+    }
+    return 0;
+}
