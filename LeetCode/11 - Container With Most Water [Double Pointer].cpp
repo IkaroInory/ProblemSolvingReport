d1608ff574ade69a4f90f0d912ca522d233b60ce
IkaroInory <ikaroinory233@gmail.com>
2021-08-20 16:12:00 +0800

    2021.8.20

---
+++ LeetCode/11 - Container With Most Water [Double Pointer].cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+/*
+* If short moved:
+*   (right - left) is smaller, min() is smaller or bigger. The area could bigger,
+* If long moved:
+*   (right - left) is smaller, min() is constant. The area must be smaller.
+*/
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int area = 0;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int temp = (right - left) * min(height[left], height[right]);
+            area = temp > area ? temp : area;
+
+            if (height[left] > height[right])right--;
+            else left++;
+        }
+        return area;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> height = {
+        {1,8,6,2,5,4,8,3,7},
+        {1,1},
+        {4,3,2,1,4},
+        {1,2,1}
+    };
+
+    for (int i = 0; i < height.size(); i++) {
+        cout << "Input: [" << height[i][0];
+        for (int j = 1; j < height[i].size(); j++) cout << "," << height[i][j];
+        cout << "]" << endl;
+        cout << "Output: " << (new Solution())->maxArea(height[i]) << endl;
+    }
+
+    return 0;
+}
---
+++ LeetCode/13 - Roman to Integer [Simulation].cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    map<char, int> roman = {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+    };
+    int romanToInt(string s) {
+        int ans = 0;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            ans += roman[s[i]];
+            if (i && roman[s[i - 1]] < roman[s[i]]) {
+                ans -= roman[s[i - 1]];
+                ans += roman[i] - roman[s[i - 1]];
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<string> height = {
+        "MCMXCIV",
+        "III",
+        "IV",
+        "IX",
+        "LVIII"
+    };
+
+    for (int i = 0; i < height.size(); i++) {
+        cout << "Input: \"" << height[i] << "\"" << endl;
+        cout << "Output: " << (new Solution())->romanToInt(height[i]) << endl;
+    }
+
+    return 0;
+}
