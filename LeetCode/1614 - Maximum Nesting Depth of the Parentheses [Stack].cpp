855db888f011d6d393ec629885b254517e5bda9e
IkaroInory <ikaroinory233@gmail.com>
2021-08-21 22:58:46 +0800

    Update.

---
+++ LeetCode/1614 - Maximum Nesting Depth of the Parentheses [Stack].cpp
@@ -0,0 +1,19 @@
+﻿//
+// vector   #include <vector>
+//
+class Solution {
+public:
+    int maxDepth(string s) {
+        vector<char> stack;
+        int length = s.length();
+        if (length <= 1) return 0;
+
+        int depth = 0;
+        for (int i = 0; i < length; i++) {
+            if (s[i] == '(') stack.push_back('C');
+            if (s[i] == ')') stack.pop_back();
+            depth = stack.size() > depth ? stack.size() : depth;
+        }
+        return depth;
+    }
+};
