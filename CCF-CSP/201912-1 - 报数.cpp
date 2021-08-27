18183a3a950f62d663ecbbe164999e9e564b2f03
IkaroInory <ikaroinory233@gmail.com>
2021-08-27 21:55:08 +0800

    Update.

---
+++ CCF-CSP/201912-1 - 报数.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isSkip(int num) {
+    if (num % 7 == 0) return true;
+
+    while (num) {
+        if (num % 10 == 7) return true;
+        num /= 10;
+    }
+
+    return false;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int location = 0;
+    int count = 0;
+    vector<int> p(4);
+    for (int i = 1; count < n; i++) {
+        if (isSkip(i)) {
+            p[location % 4]++;
+        }
+        else {
+            count++;
+        }
+        location++;
+    }
+
+    auto begin = p.begin();
+    while (begin < p.end()) {
+        cout << *begin++ << endl;
+    }
+    return 0;
+}
---
+++ CCF-CSP/202104-1 - 灰度直方图.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n, m, L;
+    cin >> n >> m >> L;
+
+    vector<int> graph(L);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            int temp;
+            cin >> temp;
+            graph[temp]++;
+        }
+    }
+    
+    auto begin = graph.begin();
+    while (begin < graph.end()) {
+        cout << *begin++ << " ";
+    }
+
+    return 0;
+}
