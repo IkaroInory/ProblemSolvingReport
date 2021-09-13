a62d1da01fadc094d685b388c35c7411c7d2e80e
IkaroInory <ikaroinory233@gmail.com>
2021-09-13 23:16:18 +0800

    Update [2021-09-13]

--- SZTU OJ/2020 Object Oriented Programming/1015 D - 动态矩阵.cpp
+++ SZTU OJ/2020 Object Oriented Programming/1015 D - 动态矩阵.cpp
@@ -1,6 +1,16 @@
 //
-// All my codes are open source to Github.
+//
+// Author   IkaroInory
+// Date     2021/9/13
+//
+// All the code of this course and submitted on Online Judge is open source on Github.
+// Welcome to reference and verification.
+//
+// Online Judge
+// http://10.11.219.17/JudgeOnline/
+// Github Repository
 // https://github.com/IkaroInory/ProblemSolvingReport/tree/master/SZTU%20OJ/2020%20Object%20Oriented%20Programming
+//
 //
 
 #include <iostream>
